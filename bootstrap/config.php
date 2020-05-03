e07c296d69a12f03f246362ae23c9c5860797a04
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-05-03 21:04:01 +0200

    enable analytics

--- bootstrap/config.php
+++ bootstrap/config.php
@@ -23,8 +23,8 @@ function config($key, $fallback = null)
                 ],
             ],
             'analytics' => [
-                'enabled' => false,
-                'domainId' => '',
+                'enabled' => true,
+                'domainId' => '9c5a80bd-016d-4100-9f0d-23aec65fe6e1',
                 'options' => '{ "localhost": false, "detailed": true }'
             ],
             'app' => [
