0477fba132c9ebb17063ee35cd3ef2f941aaae41
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-05-28 20:55:06 +0200

    add url invite handler

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     M.Modal.init(document.querySelectorAll('.modal'), {dismissible: true});
 
+    const inviteCode = new URLSearchParams(window.location.search).get('invite');
+    if (inviteCode) {
+        document.querySelector('input#invite_code').value = inviteCode;
+        M.updateTextFields();
+        M.Modal.getInstance(document.querySelector('form#register-invite')).open();
+    }
     const inviteSubmit = token => {
         document.querySelector('input#register-invite-captcha').value = token;
 
